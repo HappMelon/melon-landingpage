c6f79c0baa2bf4c653967e5282b25b58656aba23
jokereven <zjing125.cn@gmail.com>
2023-06-24 22:22:16 +0800

    fix: layout view

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,7 +5,7 @@ export default function App() {
 
 	return (
 		<div
-			className="p-4 flex flex-col gap-4 items-center justify-center font-avenir "
+			className="p-4 flex flex-col gap-4 items-center justify-center font-mono min-w-screen-xl"
 			style={{ backgroundColor: "#181127", color: "#F5F5F5" }}
 		>
 			<LandingHeader></LandingHeader>

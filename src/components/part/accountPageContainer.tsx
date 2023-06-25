439f1f72c8d6e08305c8967a408641b10d15173b
Hongdan Wang <hongdanw@umich.edu>
2023-06-25 04:18:03 -0400

    update progress 6/24

---
+++ src/components/part/accountPageContainer.tsx
@@ -0,0 +1,20 @@
+import { ArticleStack } from "@/components/part/articleStack"
+import { FollowingMillStack } from "@/components/part/followingMillStack"
+import { Flex, Spacer } from "@chakra-ui/react"
+
+export const AccountPageContainer = () => {
+	return (
+		<>
+			<Flex
+				direction="row"
+				alignItems="center"
+				justifyContent="space-between"
+				m="2vh"
+			>
+				<ArticleStack />
+				<Spacer />
+				<FollowingMillStack />
+			</Flex>
+		</>
+	)
+}

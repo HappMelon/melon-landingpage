e15f6a4430b07ee63f19f64b3a5bc9dcdda724b0
zhou jing <zjing125.cn@gmail.com>
2023-06-29 18:00:16 +0800

    Honghulu (#18)
    * add login base structure, still working on css
    
    * convert loginpage unit from px to rem
    
    ---------
    
    Co-authored-by: honghulu <honghulu.ai@gmail.com>

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -28,17 +28,17 @@ export default function Login() {
     
     return (
         <div className="login-box flex-c-c">
-            <div className='w-397px'>
+            <div className='w-24.8125rem'>
                 <form className='flex-c-c' onSubmit={onSubmit}>
-                    <div className='login-form-title pb-47px'>
-                        <img src="/images/brands/Logo.png" alt="logo" width='82px' height='82px'/>
+                    <div className='w-5.125rem h-5.125rem'>
+                        <img src="/images/brands/Logo.png" alt="logo"/>
                     </div>
-                    <div className='pb-26px'>
+                    <div className='pt-2.94rem pb-1.62rem'>
                         <label className='form-title'>
                             Sign In To Your Flare Account
                         </label>
                     </div>
-                    <div className='pb-24px'>
+                    <div className='pb-1.5rem'>
                         <label className='form-label'>Email address</label>
                         <input
                             className="form-input"
@@ -48,7 +48,7 @@ export default function Login() {
                             onChange={ changeHandle }
                         />
                     </div>
-                    <div className='pb-33px'>
+                    <div className='pb-2.06rem'>
                         <label className='form-label'>Password</label>
                         <input
                             className="form-input"
@@ -58,29 +58,29 @@ export default function Login() {
                             onChange={ changeHandle }
                         />
                     </div>
-                    <div className='pb-15px'>
+                    <div className='pb-0.94rem'>
                         <button className='form-signin-button flex-c-c'  type='submit'>
                             Sign In
                         </button>
                     </div>
-                    <div className='w-397px pb-24px'>
-                        <p className='form-para display-inline w-120px pr-11px'>
+                    <div className='w-24.8125rem pb-1.5rem'>
+                        <p className='form-para display-inline w-7.5rem pr-0.69rem'>
                             Not A Member Yet?
                         </p>
                         <a className='form-register-link' onClick={registerJump}>Register</a>
                     </div>
-                    <div className='flex-r-c pb-27px'>
+                    <div className='flex-r-c pb-1.69rem'>
                         <div>
-                            <svg width="96" height="2" viewBox="0 0 96 2" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                <line x1="8.74228e-08" y1="1" x2="96" y2="1.00001" stroke="#D9D9D9" stroke-width="2"/>
+                            <svg width="6rem" height="0.125rem" viewBox="0 0 96 2" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                <line x1="8.74228e-08" y1="1" x2="96" y2="1.00001" stroke="#D9D9D9" stroke-width="0.125rem"/>
                             </svg>
                         </div>
-                        <p className='form-para pl-13px pr-11px'>
+                        <p className='form-para pl-0.81rem pr-0.69rem'>
                             Or Connect your crypto wallet
                         </p>
                         <div>
-                            <svg width="96" height="2" viewBox="0 0 96 2" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                <line x1="8.74228e-08" y1="1" x2="96" y2="1.00001" stroke="#D9D9D9" stroke-width="2"/>
+                            <svg width="6rem" height="0.125rem" viewBox="0 0 96 2" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                <line x1="8.74228e-08" y1="1" x2="96" y2="1.00001" stroke="#D9D9D9" stroke-width="0.125rem"/>
                             </svg>
                         </div>
                     </div>

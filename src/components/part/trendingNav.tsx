782d4b8d35e0c70e89a0ea921b3679f50eb280ac
Ziwen Wang <ziwangw@gmail.com>
2023-07-10 20:15:01 -0400

    added users on trending page

--- src/components/part/trendingNav.tsx
+++ src/components/part/trendingNav.tsx
@@ -1,17 +1,35 @@
 import { TrendingArticleStack } from "@/components/part/trendingArticleStack"
 import { TrendingHotTopics } from "@/components/part/trendingHotTopics"
+import { TrendingUserStack } from "@/components/part/trendingUserStack"
 import { Box, Button, ButtonGroup, Stack } from "@chakra-ui/react"
 import { useState } from "react"
 
 export const TrendingNav = () => {
 	const [showTopics, toggleShowTopics] = useState(false)
 	const [currentTap, setCurrentTap] = useState("TrendingArticle")
+	const trendingBody = () => {
+		switch (currentTap) {
+			case "TrendingArticle":
+				return <TrendingArticleStack />
+			case "Users":
+				return <TrendingUserStack />
+			default:
+				return <TrendingArticleStack />
+		}
+	}
 	return (
 		<>
 			<Stack className="mr-auto mt-5vh flex-col">
 				<Box className="flex items-center w-full">
 					<ButtonGroup className="gap-4">
-						<Button colorScheme="blackAlpha">Most Popular</Button>
+						<Button
+							colorScheme="blackAlpha"
+							onClick={() => {
+								setCurrentTap("TrendingArticle")
+							}}
+						>
+							Most Popular
+						</Button>
 						<Button
 							colorScheme="blackAlpha"
 							onClick={() => {
@@ -20,11 +38,18 @@ export const TrendingNav = () => {
 						>
 							Hot Topics
 						</Button>
-						<Button colorScheme="blackAlpha">Users</Button>
+						<Button
+							colorScheme="blackAlpha"
+							onClick={() => {
+								setCurrentTap("Users")
+							}}
+						>
+							Users
+						</Button>
 					</ButtonGroup>
 				</Box>
 				{showTopics && <TrendingHotTopics />}
-				<TrendingArticleStack />
+				{trendingBody()}
 			</Stack>
 		</>
 	)

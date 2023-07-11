3d22729e44adece37d18647cc13c6cacecb47507
Ziwen Wang <ziwangw@gmail.com>
2023-07-10 20:15:01 -0400

    added users on trending page

--- src/components/part/trendingUserStack.tsx
+++ src/components/part/trendingUserStack.tsx
@@ -1,11 +1,12 @@
-import { TrendingArticleBox } from "@/components/part/trendingArticleBox"
+import { TrendingUserBox } from "@/components/part/trendingUserBox"
 import { Stack } from "@chakra-ui/react"
 
 export const TrendingUserStack = () => {
 	return (
 		<>
 			<Stack spacing="1.5rem" direction="column" mt="5vh">
-				<TrendingArticleBox />
+				<TrendingUserBox />
+				<TrendingUserBox />
 			</Stack>
 		</>
 	)

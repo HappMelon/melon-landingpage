dd1d335e9db68e14018842dad0cd858936acdef4
annaleee <annaleee@umich.edu>
2023-07-12 18:21:14 -0400

    update

--- src/components/part/accountPageContainer.tsx
+++ src/components/part/accountPageContainer.tsx
@@ -1,13 +1,19 @@
+import { useAccount } from "@/state/Account"
+import { useParams } from "react-router-dom"
 import { ArticleStack } from "@/components/part/articleStack"
 import { FollowingMillStack } from "@/components/part/followingMillStack"
 import { Flex, Spacer } from "@chakra-ui/react"
 
 export const AccountPageContainer = () => {
+	const { username } = useParams()
+	const { data: account } = useAccount(
+		username ? username.replace("@", "") : ""
+	)
+
 	return (
 		<>
-			<Flex
-				className="flex flex-row items-center justify-between m-2vh">
-				<ArticleStack />
+			<Flex className="flex flex-row items-center justify-between m-2vh">
+				<ArticleStack userID={account?.characterId||10} />
 				<Spacer />
 				<FollowingMillStack title="Following Mills" count={9} />
 			</Flex>
--- src/components/part/articleStack.tsx
+++ src/components/part/articleStack.tsx
@@ -1,13 +1,25 @@
 import { ArticleBox } from "@/components/part/articleBox"
-import { Stack } from "@chakra-ui/react"
+import { useArticlesID } from "@/state/Article"
 
-export const ArticleStack = () => {
+import { Stack, StackProps } from "@chakra-ui/react"
+
+interface ArticleStackProps extends StackProps {
+	userID?: number
+}
+export const ArticleStack = ({userID, ...props}:ArticleStackProps) => {
+	// according to the id, fetch the result of article
+	const { data: articles } = useArticlesID(10)
+	if(!articles||articles.list.length === 0){
+		return (
+			<Stack className="gap-1.5rem flex !flex-col mt-5vh">
+				<></>
+			</Stack>
+		)
+	}
 	return (
 		<>
 			<Stack className="gap-1.5rem flex !flex-col mt-5vh">
-				<ArticleBox />
-				<ArticleBox />
-				<ArticleBox />
+				{articles?.list.filter(article => !article.deleted).map((article,i) =><ArticleBox key={i} data={article}/>)}
 			</Stack>
 		</>
 	)

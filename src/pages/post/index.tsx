0607037f36d87f83969659dbaa54a4e4446c8138
zhou jing <zjing125.cn@gmail.com>
2023-07-13 01:13:05 +0800

    feat: Element type is invalid: expected a string (for built-in components) or a class/function (for composite components) but got: object. (#46)

--- src/pages/post/index.tsx
+++ src/pages/post/index.tsx
@@ -1,11 +1,10 @@
 import { PostPageContainer } from "@/components/part/postPageContainer"
 import { ChakraProvider } from "@chakra-ui/react"
+
 export default function App() {
 	return (
-		<>
-			<ChakraProvider>
-				<PostPageContainer />
-			</ChakraProvider>
-		</>
+		<ChakraProvider>
+			<PostPageContainer />
+		</ChakraProvider>
 	)
 }

45a694354d01c5d8a2866788140c067023908514
xzd9326 <xzd9326@gmail.com>
2023-07-12 21:52:54 -0700

    Rebase from main

--- src/components/part/postPageContainer.tsx
+++ src/components/part/postPageContainer.tsx
@@ -3,37 +3,7 @@ import { Grid, GridItem } from "@chakra-ui/react"
 import { PostPageEditor } from "@/components/part/postPageEditor"
 import { PostPageHeader } from "@/components/part/postPageHeader"
 import { PostPageNav } from "@/components/part/postPageNav"
-import { CharacterAvatar } from "@crossbell/ui"
-import { ConnectButton, useAccountCharacter } from "@flarezone/connect-kit"
 import { useNavigate } from "react-router-dom"
-function Avatar() {
-	const navigate = useNavigate()
-	const character = useAccountCharacter()
-
-	const pushAccount = () => {
-		navigate(`/@${character?.handle || ""}`)
-	}
-
-	return (
-		<ConnectButton>
-			{(status, { connect }) => {
-				if (status.isConnected) {
-					const { character } = status.account
-					return (
-						<CharacterAvatar
-							onClick={() => pushAccount()}
-							size="2.5vw"
-							character={character}
-							className="cursor-pointer"
-						/>
-					)
-				} else {
-					return <button onClick={connect}>Connect</button>
-				}
-			}}
-		</ConnectButton>
-	)
-}
 
 export const PostPageContainer = () => {
 	// eslint-disable-next-line @typescript-eslint/no-unused-vars

663784ece8ed7750519baadc0e7f76d67d9c8af4
jokereven <zjing125.cn@gmail.com>
2023-07-15 13:52:52 +0800

    feat: add trending page

--- src/components/part/trending/trendingArticleBox.tsx
+++ src/components/part/trending/trendingArticleBox.tsx
@@ -1,10 +1,11 @@
-/* eslint-disable @typescript-eslint/no-unsafe-assignment */
+import { ipfsLinkToHttpLink } from "@/share/ipfs"
 import { useAccount } from "@/state/Account"
 import { useMostPopular } from "@/state/Character"
 import { useStatus } from "@/state/Status"
 import { CharacterAvatar, Loading } from "@crossbell/ui"
 import { Note } from "crossbell"
 import { useEffect, useState } from "react"
+import ReactMarkdown from "react-markdown"
 
 const NoteWithAccount = ({ note }: { note: Note }) => {
 	const cid = note.characterId
@@ -14,19 +15,44 @@ const NoteWithAccount = ({ note }: { note: Note }) => {
 
 	const { data: h } = useMostPopular(String(n?.characterId))
 
-	console.log(h);
-
 	const accountName = h?.metadata?.content?.name ? h.metadata.content.name : ""
 	const { data: account } = useAccount(accountName)
 
+	const transformLinkUri = (uri: string) => {
+		return ipfsLinkToHttpLink(uri, { origin: "https://ipfs.io" })
+	}
+
+	const content = n?.metadata?.content?.content as string
+	const truncatedContent =
+		content?.length > 160 ? content.slice(0, 160) + "..." : content
+
 	return (
-		<div className="flex items-center gap-1rem overflow-hidden flex-nowrap">
-			<CharacterAvatar
-				className="!w-3rem !h-3rem !rounded-50% border-solid border-#fff shadow-lg object-cover"
-				size="4rem"
-				character={account}
-			/>
-		</div>
+		<button
+			className="flex flex-col w-full py-3 px-3 border-b border-gray/20 bg-hover cursor-pointer w-55rem hover:bg-#9ca3af10"
+			onClick={() => {
+				// eslint-disable-next-line @typescript-eslint/restrict-template-expressions
+				window.location.href = `/@${h?.handle}/status/${n?.characterId}-${n?.noteId}`
+			}}
+		>
+			<div className="flex">
+				<CharacterAvatar
+					className="!w-3rem !h-3rem !rounded-50% border-solid border-#fff shadow-lg object-cover"
+					size="4rem"
+					character={account}
+				/>
+				<div className="pl-0.25rem">
+					<div>{h?.handle}</div>
+					<div>{h?.metadata?.content?.name}</div>
+					<div></div>
+				</div>
+			</div>
+			<ReactMarkdown
+				className="overflow-hidden pt-1rem !text-left"
+				transformImageUri={transformLinkUri}
+			>
+				{truncatedContent}
+			</ReactMarkdown>
+		</button>
 	)
 }
 

d866aa02ec566f694fb0ed388f295bdc0e8604a2
jokereven <zjing125.cn@gmail.com>
2023-07-15 22:52:33 +0800

    feat: use jotai to store data then call contract

--- src/components/part/post/postPageEditor.tsx
+++ src/components/part/post/postPageEditor.tsx
@@ -1,4 +1,13 @@
 import {
+	Dialog,
+	DialogContent,
+	DialogDescription,
+	DialogFooter,
+	DialogHeader,
+	DialogTitle,
+	DialogTrigger,
+} from "@/components/ui/dialog"
+import {
 	Box,
 	Button,
 	ButtonGroup,
@@ -6,10 +15,67 @@ import {
 	Spacer,
 	Text,
 } from "@chakra-ui/react"
-import { useState } from "react"
+
 import { IoCloudUploadOutline } from "react-icons/io5"
 import ReactQuill from "react-quill"
 import "react-quill/dist/quill.snow.css"
+
+import { Input } from "@/components/ui/input"
+import { Label } from "@/components/ui/label"
+import { atom, useAtom } from "jotai"
+import { ChangeEvent } from "react"
+
+export const ValueAtom = atom("")
+export const BettingAtom = atom(0)
+export const EnableBettingAtom = atom(false)
+
+export function DialogBetting() {
+
+	const [betting, SetBetting] = useAtom(BettingAtom)
+	const [enable, SetEnable] = useAtom(EnableBettingAtom)
+
+	function handleBettingChange(event: ChangeEvent<HTMLInputElement>) {
+		const newValue = Number(event.target.value)
+		SetBetting(newValue)
+	}
+
+	return (
+		<Dialog>
+			<DialogTrigger asChild>
+				<Button variant="outline">Add Betting Prediction</Button>
+			</DialogTrigger>
+			<DialogContent className="sm:max-w-[425px]">
+				<DialogHeader>
+					<DialogTitle>Do Betting</DialogTitle>
+					<DialogDescription>Turn on betting when posting</DialogDescription>
+				</DialogHeader>
+				<div className="grid gap-4 py-4">
+					<div className="grid grid-cols-4 items-center gap-4">
+						<Label htmlFor="username" className="text-right">
+							Value
+						</Label>
+						<Input
+							id="value"
+							className="col-span-3"
+							placeholder="e.g. 0.01"
+							onChange={handleBettingChange}
+						></Input>
+					</div>
+				</div>
+				<DialogFooter>
+					<Button
+						type="submit"
+						onClick={() => {
+							SetEnable(true)
+						}}
+					>
+						Betting
+					</Button>
+				</DialogFooter>
+			</DialogContent>
+		</Dialog>
+	)
+}
 
 const modules = {
 	toolbar: [
@@ -19,25 +85,36 @@ const modules = {
 		["bold", "italic", "underline", "strike", "blockquote"],
 	],
 }
+
 export const PostPageEditor = () => {
-	const [value, setValue] = useState("")
+	//   const [value, setValue] = useState("");
+	//   const ValueRef = useRef<ReactQuill>(null);
+
+	//   function Post() {
+	//     console.log("HandlePost Create Post");
+	//     if (ValueRef?.current) {
+	//       console.log(ValueRef?.current.value);
+	//     }
+	//   }
+
+	const [value, setValue] = useAtom(ValueAtom)
+
 	return (
 		<Box bg="#F8F8F8" borderRadius="1rem" h="100%">
 			<ReactQuill
 				theme="snow"
-				value={value}
-				onChange={setValue}
+				onChange={(value) => setValue(value)}
 				style={{ height: "40rem" }}
 				modules={modules}
 			/>
 			<HStack>
-				<ButtonGroup>
+				<ButtonGroup className="ml-2">
 					<Button leftIcon={<IoCloudUploadOutline />}>
 						<Text>Upload Files*</Text>
 					</Button>
 					<Button leftIcon={<IoCloudUploadOutline />}>Upload Videos</Button>
 					<Spacer />
-					<Button>Add Betting Prediction</Button>
+					<DialogBetting />
 				</ButtonGroup>
 			</HStack>
 		</Box>

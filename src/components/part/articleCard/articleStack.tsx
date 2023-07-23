e66aef621d5948badf16a974e67a414b6cdbfb80
jokereven <zjing125.cn@gmail.com>
2023-07-23 23:01:38 +0800

    feat(account): detail show

--- src/components/part/articleCard/articleStack.tsx
+++ src/components/part/articleCard/articleStack.tsx
@@ -76,7 +76,7 @@ export const ArticleStack = ({
 	}
 	return (
 		<>
-			<Stack className="gap-1.5rem flex !flex-col mt-5vh">
+			<Stack className="gap-1.5rem flex !flex-col mt-5vh w-full">
 				{article
 					.filter((article) => !article.deleted)
 					.map((article, i) => (

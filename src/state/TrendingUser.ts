b5b289190279d2aec08b77786817a00628e48f8e
jokereven <zjing125.cn@gmail.com>
2023-07-24 15:49:08 +0800

    style: use swr to get popular authors data

--- src/state/TrendingUser.ts
+++ src/state/TrendingUser.ts
@@ -1,12 +1,12 @@
-import { CharacterEntity } from "crossbell"
+import { TrendingAuthors } from "@/type"
 import useSWR from "swr"
 
 const prefixUrl = "https://recommend.crossbell.io"
 const fetcher = <T>(url: string) =>
-	fetch(prefixUrl + url).then((res) => res.json() as Promise<CharacterEntity[]>)
+	fetch(prefixUrl + url).then((res) => res.json() as Promise<TrendingAuthors>)
 
 export function useTrendingUser(type: string, rand: boolean, limit: number) {
 	return useSWR(["TrendingUser", type, rand, limit], () =>
-		fetcher<CharacterEntity[]>(`/raw?type=${type}&rand=${rand}&limit=${limit}`)
+		fetcher<TrendingAuthors>(`/raw?type=${type}&rand=${rand}&limit=${limit}`)
 	)
 }
